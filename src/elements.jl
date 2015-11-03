bca9ff50c065980fa33613d2ccdc154d3052059d
Martin Holters <martin.holters@hsu-hh.de>
2015-11-03 14:02:07 +0100

    Rewrite diode's nonlinear_eq to use a let for helper variables and drop type annotations.

--- src/elements.jl
+++ src/elements.jl
@@ -8,11 +8,10 @@ voltageprobe() = Element(mi=1, pv=1, pins=[:+, :-])
 diode(is::Number, η::Number = 1) =
   Element(mv=[1,0], mi=[0,1], mq=[-1 0; 0 -1], pins=[:+, :-], nonlinear_eq =
     quote
-      v::Float64 = q[1]
-      i::Float64 = q[2]
-      ex::Float64 = exp(v*$(1./(25e-3 * η)))
-      res[1] = $(is) * (ex - 1) - i
-      J[1,1] = $(is/(25e-3 * η)) * ex
-      J[1,2] = -1
+      let v = q[1], i = q[2], ex = exp(v*$(1./(25e-3 * η)))
+        res[1] = $(is) * (ex - 1) - i
+        J[1,1] = $(is/(25e-3 * η)) * ex
+        J[1,2] = -1
+      end
     end
   )
