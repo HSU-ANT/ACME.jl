c742562116bacb476c49c3ad51581073ed2cebe2
Martin Holters <martin.holters@hsu-hh.de>
2015-11-23 11:14:24 +0100

    Add tests for degenerate cases of topomat.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using ACME
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Pathological cases for topomat:
+# two nodes, one loop branch (short-circuited) -> voltage==0, current arbitrary
+@test ACME.topomat(spzeros(Int, 2, 1)) == (sparse([1]), sparse([]))
+# two nodes, one branch between them -> voltage arbitrary, current==0
+@test ACME.topomat(sparse([1,2], [1,1], [1,-1])) == (sparse([]), sparse([1]))
