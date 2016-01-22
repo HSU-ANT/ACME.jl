ecec383b975d57b0b914349e721d7a3d9e01c84f
Martin Holters <martin.holters@hsu-hh.de>
2016-01-22 14:39:00 +0100

    Make kd-tree's Alts.delta a Vector of Vectors instead of a Matrix.
    Deletion of individual entries is now faster then deletion of rows was
    before.
    Along the way, append_alts! has been replaced with push_alt! and new
    alternatives are now added one by one.

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -61,35 +61,35 @@ end
 
 type Alts{T}
     idx::Vector{Int}
-    delta::Matrix{T}
+    delta::Vector{Vector{T}}
     delta_norms::Vector{T}
     best_dist::T
 end
 
-Alts(T, k) = Alts([1], zeros(T, k, 1), zeros(T, 1), typemax(T))
+Alts(T, k) = Alts([1], [zeros(T, k) for i in 1], zeros(T, 1), typemax(T))
 
 function pop_best_alt!(alts)
     min_idx = indmin(alts.delta_norms)
     idx = alts.idx[min_idx]
-    delta = alts.delta[:,min_idx]
+    delta = alts.delta[min_idx]
     deleteat!(alts.idx, min_idx)
     deleteat!(alts.delta_norms, min_idx)
-    alts.delta = [alts.delta[:,1:min_idx-1] alts.delta[:,min_idx+1:end]]
+    deleteat!(alts.delta, min_idx)
     return idx, delta
 end
 
-function append_alts!(alts, new_idx, new_delta)
-    append!(alts.idx, new_idx)
-    append!(alts.delta_norms, vec(sum(new_delta.^2, 1)))
-    alts.delta = [alts.delta new_delta]
+function push_alt!(alts, new_idx, new_delta)
+    push!(alts.idx, new_idx)
+    push!(alts.delta_norms, sum(new_delta.^2))
+    push!(alts.delta, new_delta)
 end
 
 function update_best_dist!(alts, dist)
     alts.best_dist = min(alts.best_dist, dist)
-    alts_keep = alts.delta_norms .< alts.best_dist
-    alts.idx = alts.idx[alts_keep]
-    alts.delta_norms = alts.delta_norms[alts_keep]
-    alts.delta = alts.delta[:,alts_keep]
+    alts_drop = find(alts.delta_norms .≥ alts.best_dist)
+    deleteat!(alts.idx, alts_drop)
+    deleteat!(alts.delta_norms, alts_drop)
+    deleteat!(alts.delta, alts_drop)
 end
 
 indnearest(tree::KDTree, p::AbstractVector, alt = Alts(eltype(p), length(p))) =
@@ -104,23 +104,21 @@ function indnearest(tree::KDTree, p::AbstractVector, max_leaves::Int,
     while l < max_leaves && ~isempty(alt.idx)
         idx, delta = pop_best_alt!(alt)
         start_depth = floor(Int, log2(idx)) + 1
-        new_alt_idx = zeros(Int, depth - start_depth + 1)
-        new_alt_delta = repmat(delta, 1, depth - start_depth + 1)
 
         for d in 1:depth - start_depth + 1
             dim = tree.cut_dim[idx]
-            new_alt_delta[dim,d] = p[dim] - tree.cut_val[idx]
+            new_alt_delta = copy(delta)
+            new_alt_delta[dim] = p[dim] - tree.cut_val[idx]
             if p[dim] ≤ tree.cut_val[idx]
-                new_alt_idx[d] = 2idx + 1
+                push_alt!(alt, 2idx+1, new_alt_delta)
                 idx *= 2
             else
-                new_alt_idx[d] = 2idx
+                push_alt!(alt, 2idx, new_alt_delta)
                 idx = 2idx + 1
             end
         end
         idx -= 2^depth - 1
 
-        append_alts!(alt, new_alt_idx, new_alt_delta)
         p_idx = tree.ps_idx[idx]
         update_best_dist!(alt, sum((p - tree.ps[:,p_idx]).^2))
 
