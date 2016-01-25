d556b98512d1d3632c2ce08beedb95523c256f98
Martin Holters <martin.holters@hsu-hh.de>
2016-01-25 09:18:12 +0100

    Add another kD-tree test.
    The new test verfies the the lookup yields the closest vector if the
    vector being searched for is not contained in the tree itself.

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,14 @@ for num = 1:50
     end
 end
 
+let ps = rand(6, 10000)
+    t = ACME.KDTree(ps)
+    p = rand(6)
+    best_p = ps[:,indmin(sumabs2(broadcast(-, ps, p),1))]
+    idx = ACME.indnearest(t, p)[1]
+    @test_approx_eq sumabs2(p - best_p) sumabs2(p - ps[:, idx])
+end
+
 # simple circuit: resistor and diode in series, driven by constant voltage,
 # chosen such that a prescribe current flows
 let i = 1e-3, r=10e3, is=1e-12
