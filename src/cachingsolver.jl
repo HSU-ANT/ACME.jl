c494d1151418fba485b3b3984b99e62441024b5f
Martin Holters <martin.holters@hsu-hh.de>
2016-02-05 11:52:09 +0100

    CachingSolver: Use same arrays for solution in tree and not yet in tree.

--- src/cachingsolver.jl
+++ src/cachingsolver.jl
@@ -5,8 +5,7 @@ type CachingSolver{BaseSolver}
     basesolver::BaseSolver
     ps_tree::KDTree{Vector{Float64}, Matrix{Float64}}
     zs::Matrix{Float64}
-    new_ps::Matrix{Float64}
-    new_zs::Matrix{Float64}
+    new_count::Int
     new_count_limit::Int
     function CachingSolver(model::DiscreteModel)
         basesolver = BaseSolver(model)
@@ -17,9 +16,7 @@ type CachingSolver{BaseSolver}
         end
         ps_tree = KDTree(zeros(np(model), 1))
         zs = reshape(z, nn(model), 1)
-        new_ps = zeros(np(model), 0)
-        new_zs = zeros(nn(model), 0)
-        return new(basesolver, ps_tree, zs, new_ps, new_zs, 50)
+        return new(basesolver, ps_tree, zs, 0, 50)
     end
 end
 
@@ -30,10 +27,11 @@ function solve(solver::CachingSolver, p, recurse=true)
 
     best_new_diff = Inf
     best_new_idx = 0
-    for i in 1:size(solver.new_ps, 2)
+    num_ps = size(solver.ps_tree.ps, 2)
+    for i in (num_ps-solver.new_count+1):num_ps
         diff = 0.
-        for j in 1:size(solver.new_ps, 1)
-            diff += (solver.new_ps[j,i] - p[j])^2
+        for j in 1:size(solver.ps_tree.ps, 1)
+            diff += (solver.ps_tree.ps[j,i] - p[j])^2
         end
         if diff < best_new_diff
             best_new_diff = diff
@@ -41,14 +39,11 @@ function solve(solver::CachingSolver, p, recurse=true)
         end
     end
 
-    if sumabs2(solver.ps_tree.ps[:,idx] - p) < best_new_diff
-        set_extrapolation_origin(solver.basesolver,
-                                 solver.ps_tree.ps[:,idx], solver.zs[:,idx])
-    else
-        set_extrapolation_origin(solver.basesolver,
-                                 solver.new_ps[:,best_new_idx],
-                                 solver.new_zs[:,best_new_idx])
+    if best_new_diff < sumabs2(solver.ps_tree.ps[:,idx] - p)
+        idx = best_new_idx
     end
+    set_extrapolation_origin(solver.basesolver,
+                             solver.ps_tree.ps[:,idx], solver.zs[:,idx])
 
     z = solve(solver.basesolver, p, 2)
     if ~hasconverged(solver.basesolver)
@@ -68,17 +63,16 @@ function solve(solver::CachingSolver, p, recurse=true)
             end
         end
         if hasconverged(solver)
-            solver.new_ps = [solver.new_ps p]
-            solver.new_zs = [solver.new_zs z]
+            solver.ps_tree.ps = [solver.ps_tree.ps p]
+            solver.zs = [solver.zs z]
+            solver.new_count += 1
         end
-        if ~isempty(solver.new_ps)
+        if solver.new_count > 0
             solver.new_count_limit -= 1
         end
-        if size(solver.new_ps, 2) > solver.new_count_limit
-            solver.ps_tree = KDTree([solver.ps_tree.ps solver.new_ps])
-            solver.zs = [solver.zs solver.new_zs]
-            solver.new_ps = zeros(size(solver.new_ps, 1), 0)
-            solver.new_zs = zeros(size(solver.new_zs, 1), 0)
+        if solver.new_count > solver.new_count_limit
+            solver.ps_tree = KDTree(solver.ps_tree.ps)
+            solver.new_count = 0
             solver.new_count_limit = 50
         end
     end
