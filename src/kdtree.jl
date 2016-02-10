be8e974fcd1bf6524d2d775209931a69fe8b07f7
Martin Holters <martin.holters@hsu-hh.de>
2016-02-10 15:51:08 +0100

    Improvements to k-d tree.
    The number of nodes in the tree is no longer padded to the next power of
    two (by replicating part of the input data). Instead, the lowest level
    of the tree is filled from left to right.
    
    Additionally, the boundaries are no longer set on the data values, but
    in the middle between them.

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -9,18 +9,27 @@ type KDTree{Tcv<:AbstractVector,Tp<:AbstractMatrix}
 end
 
 function KDTree(p::AbstractMatrix)
+    function calc_cut_idx(min_idx, max_idx)
+        N = max_idx - min_idx + 1
+        N2 = 2^floor(Int, log2(N-1))
+        if 3*N2/2 ≤ N
+            return min_idx+N2-1
+        else
+            return min_idx+N-(N2/2)-1
+        end
+    end
+
     if size(p)[2] == 0
         return KDTree{Vector{eltype(p)},typeof(p)}([], [], [], p)
     end
 
-    depth = ceil(Int, log2(size(p)[2]))
-    min_idx = zeros(Int, 2^depth-1)
-    max_idx = zeros(Int, 2^depth-1)
-    cut_idx = zeros(Int, 2^depth-1)
-    cut_dim = zeros(Int, 2^depth-1)
-    cut_val = zeros(eltype(p), 2^depth-1)
+    min_idx = zeros(Int, size(p,2)-1)
+    max_idx = zeros(Int, size(p,2)-1)
+    cut_idx = zeros(Int, size(p,2)-1)
+    cut_dim = zeros(Int, size(p,2)-1)
+    cut_val = zeros(eltype(p), size(p,2)-1)
 
-    if depth == 0
+    if size(p,2) == 1
         return KDTree{typeof(cut_val),typeof(p)}(cut_dim, cut_val, [1], p)
     end
 
@@ -29,11 +38,11 @@ function KDTree(p::AbstractMatrix)
 
     min_idx[1] = 1
     max_idx[1] = size(p)[2]
-    cut_idx[1] = div(size(p)[2]+1, 2)
+    cut_idx[1] = calc_cut_idx(min_idx[1], max_idx[1])
     cut_dim[1] = dim
-    cut_val[1] = p[dim, p_idx[cut_idx[1]]]
+    cut_val[1] = mean(p[dim, p_idx[cut_idx[1]:cut_idx[1]+1]])
 
-    for n in 2:2^depth-1
+    for n in 2:size(p,2)-1
         parent_n = div(n, 2)
         if mod(n, 2) == 0
             min_idx[n] = min_idx[parent_n]
@@ -45,15 +54,15 @@ function KDTree(p::AbstractMatrix)
         dim = indmax(var(p[:,p_idx[min_idx[n]:max_idx[n]]],2))
         idx = sortperm(vec(p[dim,p_idx[min_idx[n]:max_idx[n]]]))
         p_idx[min_idx[n]:max_idx[n]] = p_idx[idx + min_idx[n] - 1]
-        cut_idx[n] = div(min_idx[n]+max_idx[n], 2)
+        cut_idx[n] = calc_cut_idx(min_idx[n], max_idx[n])
         cut_dim[n] = dim
-        cut_val[n] = p[dim, p_idx[cut_idx[n]]]
+        cut_val[n] = mean(p[dim, p_idx[cut_idx[n]:cut_idx[n]+1]])
     end
 
-    p_idx_final = zeros(Int, 1, 2^depth)
-    for n in 1:2^depth
-        parent_n = div(n+2^depth-1, 2);
-        if mod(n,2) == 1
+    p_idx_final = zeros(Int, 1, size(p,2))
+    for n in 1:size(p,2)
+        parent_n = div(n+size(p,2)-1, 2);
+        if mod(n+size(p,2),2) == 1
             p_idx_final[n] = p_idx[min_idx[parent_n]]
         else
             p_idx_final[n] = p_idx[max_idx[parent_n]]
@@ -121,8 +130,6 @@ indnearest(tree::KDTree, p::AbstractVector, alt = Alts(p)) =
 
 function indnearest(tree::KDTree, p::AbstractVector, max_leaves::Int,
                     alt = Alts(p))
-    depth = round(Int, log2(length(tree.cut_dim)+1))
-
     l = 0
     p_idx = 0
     while l < max_leaves && ~isempty(alt.idx)
@@ -132,9 +139,7 @@ function indnearest(tree::KDTree, p::AbstractVector, max_leaves::Int,
         delta_norm = alt.delta_norms[best_pos]
         deletepos!(alt, best_pos)
 
-        start_depth = floor(Int, log2(idx)) + 1
-
-        for d in 1:depth - start_depth + 1
+        while idx ≤ length(tree.cut_dim)
             dim = tree.cut_dim[idx]
             new_alt_delta_norm = delta_norm - delta[dim]^2 + (p[dim] - tree.cut_val[idx])^2
             if new_alt_delta_norm < alt.best_dist
@@ -152,7 +157,7 @@ function indnearest(tree::KDTree, p::AbstractVector, max_leaves::Int,
                 idx = 2idx + 1
             end
         end
-        idx -= 2^depth - 1
+        idx -= length(tree.cut_dim)
 
         p_idx = tree.ps_idx[idx]
         dist = 0.
