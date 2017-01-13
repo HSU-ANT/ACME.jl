bf3982f0c4875377fa58ed81641c37e2a100d905
Martin Holters <martin.holters@hsu-hh.de>
2017-01-13 11:19:49 +0100

    Avoid using `@test_approx_eq` (deprecated in JuliaLang/julia#19880)

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,10 +19,10 @@ let solver = ACME.LinearSolver(3)
     x = rand(3)
     y = similar(x)
     ACME.solve!(solver, y, x)
-    @test_approx_eq A*y x
+    @test A*y ≈ x
     copy!(y, x)
     ACME.solve!(solver, y, y)
-    @test_approx_eq A*y x
+    @test A*y ≈ x
     @test_throws DimensionMismatch ACME.setlhs!(solver, zeros(2, 3))
     @test_throws DimensionMismatch ACME.setlhs!(solver, zeros(3, 4))
     @test_throws DimensionMismatch ACME.setlhs!(solver, zeros(4, 4))
@@ -87,7 +87,7 @@ let ps = rand(6, 10000)
     p = rand(6)
     best_p = ps[:,indmin(sum(abs2, broadcast(-, ps, p),1))]
     idx = ACME.indnearest(t, p)
-    @test_approx_eq sum(abs2, p - best_p) sum(abs2, p - ps[:, idx])
+    @test sum(abs2, p - best_p) ≈ sum(abs2, p - ps[:, idx])
 end
 
 let nleq = ACME.ParametricNonLinEq((res, J, scratch, z) ->
@@ -150,7 +150,7 @@ function checksteady!(model)
     x_steady = steadystate!(model)
     ACME.set_resabs2tol!(model.solver, 1e-25)
     run!(model, zeros(1, 1))
-    @test_approx_eq model.x x_steady
+    @test model.x ≈ x_steady
 end
 
 include("../examples/sallenkey.jl")
